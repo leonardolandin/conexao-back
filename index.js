c6bf2919fa06964393ddbf22fc30e63c15f8fce8
LeoLandin <leolandin123@gmail.com>
2021-08-19 17:27:16 -0300

    basic

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('./src/config');
+const port = 8080
+require('dotenv');
+
+
+app.listen(port, () => {
+    
+})
---
+++ src/config/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const routes = require('../router');
+const cors = require('cors');
+
+app.use(bodyParser.json({
+    limit: '10mb', 
+    extended: true
+}));
+
+let whiteList = [];
+
+if(process.env.CORS_ORIGINS) {
+    whiteList = JSON.parse(process.env.CORS_ORIGINS);
+}
+
+//let corsOptions = {
+//    origin: function(origin, callback) {
+//        if(whiteList.indexOf(origin) !== -1) {
+//            return callback(null, true);
+//        } else {
+//            return callback(null, false);
+//        }
+//    },
+//    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS']
+//}
+
+module.exports = app;
