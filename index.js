11f384ef8d924a97ddcf5c17d4d33afe9cb7cabf
LeoLandin <leolandin123@gmail.com>
2021-09-13 22:18:45 -0300

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./src/config');
 const MongoDB = require('./src/database');
 require('dotenv');
 
-app.listen(process.env.PORT , () => {
+app.listen(process.env.PORT || 3000, () => {
     MongoDB.connect().then(db => {
         if(db) {
             process.on('SIGINT', function() {
